68065240980274c3795896e67f6fc56bfbb9df04
naeemul-online <naeemul.online@gmail.com>
2025-05-25 05:43:05 +0300

    Solved: Problems

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,194 @@
 -- Active: 1747482091632@@127.0.0.1@5430@conservation_db@public
 CREATE DATABASE conservation_db;
+
+--creating ranger table
+CREATE TABLE
+    rangers (
+        ranger_id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        region VARCHAR(100) NOT NULL
+    )
+    -- inserting ranger data
+INSERT INTO
+    rangers (name, region)
+VALUES
+    ('Alice Green', 'Northern Hills'),
+    ('Bob White', 'River Delta'),
+    ('Carol King', 'Mountain Range');
+
+-- creating species table
+CREATE TABLE
+    species (
+        species_id SERIAL PRIMARY KEY,
+        common_name VARCHAR(100) NOT NULL,
+        scientific_name VARCHAR(100) NOT NULL,
+        discovery_date DATE,
+        conservation_status VARCHAR(50) CHECK (
+            conservation_status IN ('Endangered', 'Vulnerable', 'Historic')
+        )
+    )
+    -- inserting into species table data
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES
+    (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
+
+-- sightings table
+CREATE TABLE
+    sightings (
+        sighting_id SERIAL PRIMARY KEY,
+        species_id INT REFERENCES species (species_id),
+        ranger_id INT REFERENCES rangers (ranger_id),
+        sighting_time TIMESTAMP,
+        location VARCHAR(100),
+        notes TEXT
+    );
+
+-- inserting sightings data
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES
+    (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
+
+SELECT
+    *
+FROM
+    sightings;
+
+-- Problem 1: Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+INSERT INTO
+    rangers (name, region)
+VALUES
+    ('Derek Fox', 'Coastal Plains');
+
+-- Problem 2: Count unique species ever sighted.
+SELECT
+    COUNT(DISTINCT species_id) AS unique_species_count
+FROM
+    sightings;
+
+-- Problem 3: Find all sightings where the location includes "Pass".
+SELECT
+    *
+FROM
+    sightings
+WHERE
+    location LIKE '%Pass%';
+
+-- Problem 4: List each ranger's name and their total number of sightings.
+SELECT
+    r.name,
+    count(s.sighting_id) as total_sighting
+FROM
+    rangers r
+    INNER JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY
+    r.name
+ORDER BY
+    r.name;
+
+-- Problem 5:  List species that have never been sighted.
+SELECT
+    s.common_name
+FROM
+    species s
+    LEFT JOIN sightings si ON s.species_id = si.species_id
+WHERE
+    si.species_id IS NULL;
+
+-- Problem 6: Show the most recent 2 sightings.
+SELECT
+    sp.common_name,
+    si.sighting_time,
+    r.name
+FROM
+    sightings si
+    JOIN species sp ON si.species_id = sp.species_id
+    JOIN rangers r On si.ranger_id = r.ranger_id
+ORDER BY
+    si.sighting_time DESC
+LIMIT
+    2;
+
+-- Problem 7: Update all species discovered before year 1800 to have status 'Historic'.
+UPDATE species
+SET
+    conservation_status = 'Historic'
+WHERE
+    discovery_date < '1800-01-01';
+
+-- Problem 8: Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+SELECT sighting_id,
+CASE 
+    WHEN sighting_time::time < '12:00:00' THEN 'Morning'  
+    WHEN sighting_time::time >= '12:00:00' AND sighting_time::time < '17:00:00' THEN 'Afternoon'  
+    ELSE  'Evening'
+END AS time_of_day
+FROM sightings;
+
+
+-- Problem 9: Delete rangers who have never sighted any species
+DELETE FROM rangers WHERE NOT EXISTS (
+    SELECT 1 FROM sightings WHERE sightings.ranger_id = rangers.ranger_id
+) ;
