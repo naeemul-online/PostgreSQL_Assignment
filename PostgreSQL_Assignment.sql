c855ec5f496959b2619654c2b6ac01cdf2e2a4e8
naeemul-online <naeemul.online@gmail.com>
2025-05-26 18:44:50 +0300

    Readme file updated

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -75,6 +75,7 @@ CREATE TABLE
 -- inserting sightings data
 INSERT INTO
     sightings (
+        sighting_id,
         species_id,
         ranger_id,
         location,
@@ -82,28 +83,28 @@ INSERT INTO
         notes
     )
 VALUES
-    (
+    (   1,
         1,
         1,
         'Peak Ridge',
         '2024-05-10 07:45:00',
         'Camera trap image captured'
     ),
-    (
+    (   2,
         2,
         2,
         'Bankwood Area',
         '2024-05-12 16:20:00',
         'Juvenile seen'
     ),
-    (
+    (   3,
         3,
         3,
         'Bamboo Grove East',
         '2024-05-15 09:10:00',
         'Feeding observed'
     ),
-    (
+    (   4,
         1,
         2,
         'Snowfall Pass',
@@ -111,11 +112,7 @@ VALUES
         NULL
     );
 
-SELECT
-    *
-FROM
-    sightings;
-
+DROP TABLE sightings;
 -- Problem 1: Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 INSERT INTO
     rangers (name, region)
